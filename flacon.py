a95d42af79bb8d9fd0c602c34156c5668a4fad6a
kandapagari <785pavan@gmail.com>
2023-07-19 17:32:41 +0200

    feat(falcon): added new file flacon.py
    Added a new file `flacon.py` which contains a class `Falcon` that extends `torch.nn.Module`. The class initializes a tokenizer and a pipeline using the `transformers` library. The `forward` method generates text using the pipeline and returns the generated text. The `dry_run` method calls the `forward` method with a sample text and prints the result. The `__main__` block creates an instance of `Falcon` and calls the `dry_run` method.
    
    chore(requirements): updated requirements.txt
    
    Updated the `requirements.txt` file

---
+++ flacon.py
@@ -0,0 +1,40 @@
+from transformers import AutoTokenizer
+import transformers
+import torch
+
+
+class Falcon(torch.nn.Module):
+
+    def __init__(self, model_name="tiiuae/falcon-7b-instruct"):
+        self.tokenizer = AutoTokenizer.from_pretrained(model_name)
+        self.pipeline = transformers.pipeline(
+            "text-generation",
+            model=model_name,
+            tokenizer=self.tokenizer,
+            torch_dtype=torch.bfloat16,
+            trust_remote_code=True,
+            device_map="auto",
+        )
+
+    def forward(self, text):
+        sequences = self.pipeline(
+            text,
+            max_length=200,
+            do_sample=True,
+            top_k=10,
+            num_return_sequences=1,
+            eos_token_id=self.tokenizer.eos_token_id,
+        )
+        results = ''
+        for seq in sequences:
+            results += seq['generated_text']
+        return results
+
+    def dry_run(self):
+        results = self.forward("Write a poem about Valencia.")
+        print(f"Result: {results}")
+
+
+if __name__ == "__main__":
+    model = Falcon()
+    model.dry_run()
