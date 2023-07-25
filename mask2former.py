e74adb0a5e5112a3628b42f50b5261c5e681da90
kandapagari <785pavan@gmail.com>
2023-07-25 16:51:35 +0200

    feat(mask2former): added Mask2Former class
    Added a new file `mask2former.py` which contains a class `Mask2Former` that extends `torch.nn.Module`. The class initializes a `Mask2FormerForUniversalSegmentation` model and an `AutoImageProcessor` processor. It also defines a `forward` method that takes an image as input and returns the result of processing the image with the model. The `labels` property returns the labels from a file `coco-labels-2014_2017.txt`. The script also includes a main block that creates an instance of `Mask2Former`, downloads

---
+++ mask2former.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+import requests
+import torch
+from PIL import Image
+from transformers import (AutoImageProcessor,
+                          Mask2FormerForUniversalSegmentation)
+
+
+class Mask2Former(torch.nn.Module):
+
+    def __init__(self, *args, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
+        self.processor = AutoImageProcessor.from_pretrained(
+            "facebook/mask2former-swin-tiny-coco-instance")
+        self.model = Mask2FormerForUniversalSegmentation.from_pretrained(
+            "facebook/mask2former-swin-tiny-coco-instance")
+
+    @property
+    def labels(self):
+        return open("coco-labels-2014_2017.txt").read().splitlines()
+
+    def forward(self, image):
+        inputs = self.processor(images=image, return_tensors="pt")
+        with torch.no_grad():
+            outputs = self.model(**inputs)
+
+        result = self.processor.post_process_instance_segmentation(
+            outputs,
+            overlap_mask_area_threshold=0.9,
+            mask_threshold=0.9,
+            threshold=0.95,
+            target_sizes=[image.size[::-1]])[0]
+
+        return result
+
+
+if __name__ == "__main__":
+    mask2former = Mask2Former()
+    url = "http://images.cocodataset.org/test2017/000000000674.jpg"
+    image = Image.open(requests.get(url, stream=True).raw)
+    instance_map = mask2former.forward(image)
+    print(instance_map)
