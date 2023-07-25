da26113ceb633c5d4bebb2340f0aab6626865abd
kandapagari <785pavan@gmail.com>
2023-07-25 17:21:26 +0200

    feat(oneformer): added OneFormer class for image segmentation
    This commit adds a new file called "oneformer.py" which contains a class called "OneFormer" that extends the torch.nn.Module class. The OneFormer class includes methods for processing images using the OneFormerForUniversalSegmentation model and returns semantic, instance, and panoptic segmentation maps. It also includes a property for retrieving the labels used by the model. An example usage of the OneFormer class to process an image and print the segmentation maps is included in the code.

---
+++ oneformer.py
@@ -0,0 +1,56 @@
+# -*- coding: utf-8 -*-
+from enum import Enum
+
+import requests
+import torch
+from PIL import Image
+from transformers import OneFormerForUniversalSegmentation, OneFormerProcessor
+
+
+class TaskInput(Enum):
+    semantic = "semantic"
+    instance = "instance"
+    panoptic = "panoptic"
+
+
+class OneFormer(torch.nn.Module):
+
+    def __init__(self, *args, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
+        self.processor = OneFormerProcessor.from_pretrained(
+            "shi-labs/oneformer_coco_swin_large")
+        self.model = OneFormerForUniversalSegmentation.from_pretrained(
+            "shi-labs/oneformer_coco_swin_large")
+        self.func_map = dict(
+            semantic=self.processor.post_process_semantic_segmentation,
+            instance=self.processor.post_process_instance_segmentation,
+            panoptic=self.processor.post_process_panoptic_segmentation)
+
+    @property
+    def labels(self):
+        return open("coco-labels-2014_2017.txt").read().splitlines()
+
+    def forward(self, image, task: TaskInput = TaskInput.instance):
+        inputs = self.processor(images=image,
+                                task_inputs=[task.value],
+                                return_tensors="pt")
+        with torch.no_grad():
+            outputs = self.model(**inputs)
+
+        result = self.func_map[task.value](outputs,
+                                           target_sizes=[image.size[::-1]])[0]
+
+        return result
+
+
+if __name__ == "__main__":
+    oneformer = OneFormer()
+    url = "http://images.cocodataset.org/test2017/000000000674.jpg"
+    # url = 'https://huggingface.co/datasets/shi-labs/oneformer_demo/blob/main/coco.jpeg'
+    image = Image.open(requests.get(url, stream=True).raw)
+    instance_map = oneformer.forward(image, task=TaskInput.instance)
+    semantic_map = oneformer.forward(image, task=TaskInput.semantic)
+    panoptic_map = oneformer.forward(image, task=TaskInput.panoptic)
+    print(semantic_map)
+    print(instance_map)
+    print(panoptic_map)
