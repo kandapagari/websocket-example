b4c689764a71738c4d999001151e23c1c84ce85b
kandapagari <785pavan@gmail.com>
2023-09-27 11:09:23 +0200

    feat(models): added T2iAdapterCanny model
    The T2iAdapterCanny model is now included in the models module for import accessibility.

--- models/__init__.py
+++ models/__init__.py
@@ -5,9 +5,10 @@ from flacon import Flacon
 from git_model import GitModel
 from mask2former import Mask2Former
 from oneformer import OneFormer
+from t2i_adapter_canny import T2iAdapterCanny
 from yolos_tiny import YoloTiny
 
 __all__ = [
     "DepthEstimator", "Flacon", "GitModel", "Mask2Former", "OneFormer", "Detr",
-    "YoloTiny"
+    "YoloTiny", "T2iAdapterCanny"
 ]
